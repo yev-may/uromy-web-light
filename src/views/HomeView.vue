f094c9f8a17c93e2dd73db993a425b47b43f49e2
yev-may <yevhenii.may@gmail.com>
2023-09-21 17:36:57 +0300

    Update home page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,19 @@
+<script setup lang="ts">
+import router from "@/router";
+</script>
 <template>
   <v-container class="fill-height">
-    <v-responsive class="align-center text-center fill-height">
-
-      <v-img height="300" src="@/assets/logo.svg" />
-
+    <v-responsive class="fill-height align-center text-center">
+      <v-img
+        height="300"
+        src="@/assets/logo.svg"
+      />
       <div class="text-body-2 font-weight-light mb-n1">Welcome to</div>
-
       <h1 class="text-h2 font-weight-bold">Uromy</h1>
-
+      <v-btn
+        class="mt-5"
+        @click="router.push({ name: 'study' })"
+      >Get started</v-btn>
     </v-responsive>
   </v-container>
 </template>
