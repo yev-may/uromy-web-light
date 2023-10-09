0114c202ace2a19ceaf07bdb1cb1c9f978b14aa9
yev-may <yevhenii.may@gmail.com>
2023-10-09 18:26:18 +0300

    Update home page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,11 +5,16 @@ import router from "@/router";
   <v-container class="fill-height">
     <v-responsive class="fill-height align-center text-center">
       <v-img
-        height="300"
+        height="200"
         src="@/assets/logo.svg"
       />
       <div class="text-body-2 font-weight-light mb-n1">Welcome to</div>
       <h1 class="text-h2 font-weight-bold">Uromy</h1>
+      <div class="mt-5">
+        <p>Uromy is created to make your study more productive</p>
+        <p>It automates some aspects of <a target="_blank" href="https://en.wikipedia.org/wiki/Spaced_repetition">Spaced Repetition technique</a>,</p>
+        <p>So You can concentrate on material, while Uromy makes routine part</p>
+      </div>
       <v-btn
         class="mt-5"
         @click="router.push({ name: 'study' })"
