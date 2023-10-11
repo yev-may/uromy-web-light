550ff4edcf960ff45c52bc58a59f9057083963c4
yev-may <yevhenii.may@gmail.com>
2023-10-11 15:19:37 +0300

    Fix ts

--- src/core/dao/abstractDao.ts
+++ src/core/dao/abstractDao.ts
@@ -1,7 +1,8 @@
 import { storage_findEntity, storage_saveEntity } from "@/core/dao/storage/storage";
 
 export function dao_generateNextId(key: string): number {
-  const nextValue: number = storage_findEntity<number>(key) + 1 || 0
+  const storageValue: number | null = storage_findEntity<number>(key)
+  const nextValue: number = storageValue ? storageValue + 1 : 0
   storage_saveEntity<number>(key, nextValue)
   return nextValue
 }
