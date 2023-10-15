c67b94cddb47ffd85de1a31265d30f65ece581be
yev-may <yevhenii.may@gmail.com>
2023-10-15 17:03:17 +0300

    Add card preview on card panel

--- src/components/CardPanel.vue
+++ src/components/CardPanel.vue
@@ -1,33 +1,71 @@
 <script setup lang="ts">
 import '@vueup/vue-quill/dist/vue-quill.snow.css'
-import { clearHtmlTags } from "@/core/service/htmlService"
 import router from "@/router"
+import CardSide from "@/components/CardSide.vue";
 
 defineProps<{
   card: Card,
   deleteFunction: Function,
 }>()
 
-const PREVIEW_LENGTH: number = 15
-const formatPreview = (question: string):string => {
-  return question.length <= PREVIEW_LENGTH
-    ? question : question.slice(0, PREVIEW_LENGTH - 3) + '...'
-}
 </script>
 
 <template>
-  <v-sheet
-    class="card-panel ql-editor d-flex justify-space-between mb-3"
-    color="grey-lighten-3"
-    border
-    rounded
-  >
-    <div class="d-flex align-center">
-      {{ formatPreview(clearHtmlTags(card.question)) }}
+  <div class="card-preview">
+    <div class="content">
+      <CardSide
+        :content="card.question"
+      />
+      <div class="actions">
+        <v-btn class="mt-3 mr-3" @click="router.push({ name: 'edit-card', params: { 'card': card.id }})">Edit</v-btn>
+        <v-btn class="mt-3 mr-3" @click="deleteFunction">Delete</v-btn>
+      </div>
     </div>
-    <div>
-      <v-btn class="mr-3" @click="router.push({ name: 'edit-card', params: { 'card': card.id }})">Edit</v-btn>
-      <v-btn @click="deleteFunction">Delete</v-btn>
-    </div>
-  </v-sheet>
+    <div class="preview-hider" />
+  </div>
 </template>
+
+<style scoped>
+.card-preview {
+  position: relative;
+  max-height: 80px;
+  overflow: hidden;
+}
+
+.card-preview > .content > .actions {
+  display: none;
+}
+
+.card-preview:hover > .content > .actions {
+  display: flex;
+}
+
+.card-preview:hover {
+  max-height: none;
+}
+
+.card-preview:hover > .preview-hider {
+  display: none;
+}
+
+.preview-hider {
+  position: absolute;
+  bottom: 0;
+  z-index: 1;
+  height: 20px;
+  width: 100%;
+  background: linear-gradient(to bottom, rgba(0, 0, 0, 0) 0%, white 100%);
+}
+
+.content {
+  position: relative;
+}
+
+.actions {
+  position: absolute;
+  top: 0;
+  right: 0;
+  display: flex;
+  flex-direction: column;
+}
+</style>
