481ae528f3237c689267047625209b5fbe4a632f
yev-may <yevhenii.may@gmail.com>
2023-10-16 11:26:26 +0300

    Fix sequence generation

--- src/core/dao/abstractDao.ts
+++ src/core/dao/abstractDao.ts
@@ -2,7 +2,9 @@ import { storage_findEntity, storage_saveEntity } from "@/core/dao/storage/stora
 
 export function dao_generateNextId(key: string): number {
   const storageValue: number | null = storage_findEntity<number>(key)
-  const nextValue: number = storageValue ? storageValue + 1 : 0
+  console.log(storageValue)
+  const nextValue: number = storageValue ? storageValue + 1 : 1
+  console.log(nextValue)
   storage_saveEntity<number>(key, nextValue)
   return nextValue
 }
