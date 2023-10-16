68a78db6a6beda7395454d2925af0acea64c2c40
yev-may <yevhenii.may@gmail.com>
2023-10-16 10:47:51 +0300

    Extract preview component

--- src/components/CardPanel.vue
+++ src/components/CardPanel.vue
@@ -1,7 +1,8 @@
 <script setup lang="ts">
+import CardSide from '@/components/CardSide.vue'
+import Preview from '@/components/Preview.vue'
 import '@vueup/vue-quill/dist/vue-quill.snow.css'
-import router from "@/router"
-import CardSide from "@/components/CardSide.vue";
+import router from '@/router'
 
 defineProps<{
   card: Card,
@@ -11,69 +12,36 @@ defineProps<{
 </script>
 
 <template>
-  <div class="card-preview">
-    <div class="content">
-      <CardSide
-        :content="card.question"
+  <Preview>
+    <CardSide
+      :content="card.question"
+    />
+    <div class="preview__actions">
+      <v-btn
+        class="mt-3 mr-3"
+        icon="mdi-trash-can-outline"
+        density="comfortable"
+        @click="deleteFunction"
       />
-      <div class="actions">
-        <v-btn
-          class="mt-3 mr-3"
-          icon="mdi-trash-can-outline"
-          density="comfortable"
-          @click="deleteFunction"
-        />
-        <v-btn
-          class="mt-3 mr-3 ma-0"
-          icon="mdi-cog-outline"
-          density="comfortable"
-          @click="router.push({ name: 'edit-card', params: { 'card': card.id }})"
-        />
-      </div>
+      <v-btn
+        class="mt-3 mr-3 ma-0"
+        icon="mdi-cog-outline"
+        density="comfortable"
+        @click="router.push({ name: 'edit-card', params: { 'card': card.id }})"
+      />
     </div>
-    <div class="preview-hider" />
-  </div>
+  </Preview>
 </template>
 
 <style lang="css" scoped>
-.card-preview {
-  position: relative;
-  max-height: 80px;
-  overflow: hidden;
+.preview__actions {
+  display: none;
+}
 
-  .content {
-    position: relative;
-
-    .actions {
-      display: none;
-    }
-  }
-
-  .preview-hider {
-    position: absolute;
-    bottom: 0;
-    z-index: 1;
-    height: 20px;
-    width: 100%;
-    background: linear-gradient(to bottom, rgba(0, 0, 0, 0) 0%, white 100%);
-  }
-
-  &:hover {
-    max-height: none;
-
-    .content {
-
-      .actions {
-        position: absolute;
-        top: 0;
-        right: 0;
-        display: flex;
-      }
-    }
-
-    .preview-hider {
-      display: none;
-    }
-  }
+.preview__content:hover > .preview__actions {
+  position: absolute;
+  top: 0;
+  right: 0;
+  display: flex;
 }
 </style>
---
+++ src/components/Preview.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="preview">
+    <div class="preview__content">
+      <slot />
+    </div>
+    <div class="preview__hider" />
+  </div>
+</template>
+
+<style lang="css" scoped>
+.preview {
+  position: relative;
+  overflow: hidden;
+  max-height: 80px;
+
+  .preview__content {
+    position: relative;
+  }
+
+  .preview__hider {
+    position: absolute;
+    z-index: 1;
+    bottom: 0;
+    width: 100%;
+    height: 20px;
+    background: linear-gradient(to bottom, rgba(0, 0, 0, 0) 0%, white 100%);
+  }
+
+  &:hover {
+    max-height: none;
+
+    .preview__hider {
+      display: none;
+    }
+  }
+}
+</style>
