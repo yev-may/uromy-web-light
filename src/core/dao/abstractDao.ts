9b0d1a14a9d9ec0f390cb8fa717021b1b66db4a9
yev-may <yevhenii.may@gmail.com>
2023-10-17 15:02:43 +0300

    Remove dev logger

--- src/core/dao/abstractDao.ts
+++ src/core/dao/abstractDao.ts
@@ -2,9 +2,7 @@ import { storage_findEntity, storage_saveEntity } from "@/core/dao/storage/stora
 
 export function dao_generateNextId(key: string): number {
   const storageValue: number | null = storage_findEntity<number>(key)
-  console.log(storageValue)
   const nextValue: number = storageValue ? storageValue + 1 : 1
-  console.log(nextValue)
   storage_saveEntity<number>(key, nextValue)
   return nextValue
 }
